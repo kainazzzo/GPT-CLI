8469019e7e7341dbaeff098741cd485a34e93f28
dom@datti.net <dom@datti.net>
2023-04-06 12:47:19 -0400

    Fixed the release script

--- release.bat
+++ release.bat
@@ -18,13 +18,23 @@ if not "%SUFFIX%"=="" (
 )
 
 echo Updating project file...
-powershell -Command "(Get-Content .\gpt.csproj) -replace '<FileVersion>[\d\.]+<\/FileVersion>', '<FileVersion>%FILE_VERSION%<\/FileVersion>' -replace '<AssemblyVersion>[\d\.]+<\/AssemblyVersion>', '<AssemblyVersion>%ASSEMBLY_VERSION%<\/AssemblyVersion>' -replace '<Version>[\d\.\-a-zA-Z]+<\/Version>', '<Version>%FILE_VERSION%%SUFFIX%<\/Version>' | Set-Content .\gpt.csproj"
+powershell -Command "(Get-Content .\gpt.csproj) -replace '(<FileVersion>)[\d\.]+(</FileVersion>)', '`${1}%FILE_VERSION%`${2}' -replace '(<AssemblyVersion>)[\d\.]+(</AssemblyVersion>)', '`${1}%ASSEMBLY_VERSION%`${2}' -replace '(<Version>)[\d\.\-a-zA-Z]+(</Version>)', '`${1}%FILE_VERSION%%SUFFIX%`${2}' | Set-Content .\gpt.csproj"
+
+echo Committing the changes...
+echo git add .\gpt.csproj
+git add .\gpt.csproj
+
+echo git commit -m "Releasing v%FILE_VERSION%%SUFFIX%"
+git commit -m "Releasing v%FILE_VERSION%%SUFFIX%"
 
 echo Tagging the repository...
 echo git tag v%FILE_VERSION%%SUFFIX%
 git tag v%FILE_VERSION%%SUFFIX%
 
-echo Pushing the tag to the remote repository...
+echo Pushing the commit and tag to the remote repository...
+echo git push origin
+git push origin
+
 echo git push origin v%FILE_VERSION%%SUFFIX%
 git push origin v%FILE_VERSION%%SUFFIX%
 
